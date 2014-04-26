59521112f6c1d800d14bb35f6277fe6e188e80f2
Guillermo Gutiérrez <guillegr123@gmail.com>
2014-04-25 18:16:30 -0600

    Instalación de paquete NuGet AngularJS, y prueba preliminar de AngularJS.

---
+++ SampleTraders/Scripts/controllers.js
@@ -0,0 +1,18 @@
+﻿var sampleTradersApp = angular.module('SampleTradersApp', []);
+
+sampleTradersApp.controller('ProductListCtrl', function ($scope) {
+    $scope.phones = [
+    {
+        'name': 'Nexus S',
+        'snippet': 'Fast just got faster with Nexus S.'
+    },
+    {
+        'name': 'Motorola XOOM™ with Wi-Fi',
+        'snippet': 'The Next, Next Generation tablet.'
+    },
+    {
+        'name': 'MOTOROLA XOOM™',
+        'snippet': 'The Next, Next Generation tablet.'
+    }
+    ];
+});
