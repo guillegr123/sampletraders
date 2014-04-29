6713065f0f24adf01124aa2f466c6f910998d75c
Guillermo Gutiérrez <guillegr123@gmail.com>
2014-04-29 16:41:03 -0600

    Adicion de directiva "valid-number", para permitir ingresar solamente números en campo de cantidad de formulario de producto.

--- SampleTraders/Scripts/app.js
+++ SampleTraders/Scripts/app.js
@@ -2,3 +2,35 @@
     'sampleTradersControllers',
     'sampleTradersServices'
 ]);
+
+// Number-only directive: http://stackoverflow.com/a/19063323/806975
+sampleTradersApp.directive('validNumber', function() {
+    return {
+        require: '?ngModel',
+        link: function(scope, element, attrs, ngModelCtrl) {
+            if(!ngModelCtrl) {
+                return; 
+            }
+
+            ngModelCtrl.$parsers.push(function (val) {
+                var clean;
+                if (val) {
+                    clean = val.replace(/[^0-9]+/g, '');
+                } else {
+                    clean = '0';
+                }
+                if (val !== clean) {
+                    ngModelCtrl.$setViewValue(clean);
+                    ngModelCtrl.$render();
+                }
+                return clean;
+            });
+
+            element.bind('keypress', function(event) {
+                if(event.keyCode === 32) {
+                    event.preventDefault();
+                }
+            });
+        }
+    };
+});
